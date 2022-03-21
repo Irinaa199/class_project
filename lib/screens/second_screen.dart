7db8f6d8761e58c8706e8bca50809bca8cf99f95
Irinaa199 <tirinaa19@gmail.com>
2022-03-21 10:50:07 +0800

    navigated back(pop)

--- lib/screens/second_screen.dart
+++ lib/screens/second_screen.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
-void main(List<String> args) {
-  
-}
+
+void main(List<String> args) {}
+
 class SecondScreen extends StatelessWidget {
-  const SecondScreen({ Key? key }) : super(key: key);
+  const SecondScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( ),
+      appBar: AppBar(),
       body: Container(
         color: Colors.indigo,
+        child:Center(
+        child: ElevatedButton(
+          
+            onPressed: () {
+              Navigator.pop(context);
+            },
+                child: Text("Go back"),
+            
+        
+            ),
       ),
+    )
     );
   }
 }
